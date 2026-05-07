2c6b65d78534b45fee799ccd8b7c733be4a586a2
lilithz16 <lil.lithz1616@gmail.com>
2026-05-07 13:29:11 +0200

    feat: recibe datos del formulario

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -19,6 +19,27 @@ def health_check():
     return jsonify({
         "success": True,
         "message": "Backend funcionando correctamente"
+    }), 200
+
+
+@app.route("/api/solicitud", methods=["POST"])
+def recibir_solicitud():
+    datos_candidato = {
+        "nombre": request.form.get("nombre"),
+        "apellidos": request.form.get("apellidos"),
+        "email": request.form.get("email"),
+        "telefono": request.form.get("telefono"),
+        "puesto": request.form.get("puesto"),
+        "jornada": request.form.get("jornada"),
+        "ubicacion": request.form.get("ubicacion"),
+        "mensaje": request.form.get("mensaje"),
+        "privacidad": request.form.get("privacidad")
+    }
+
+    return jsonify({
+        "success": True,
+        "message": "Datos recibidos correctamente",
+        "datos_recibidos": datos_candidato
     }), 200
 
 
